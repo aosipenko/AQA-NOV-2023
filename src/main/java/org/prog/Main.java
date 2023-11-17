4af41db4d4081e0e806e53e0c58e45119e98b467
aosipenko <aoskiev@gmail.com>
2023-11-17 20:12:16 +0100

    Session 2: Java essentials pt 2

--- src/main/java/org/prog/Main.java
+++ src/main/java/org/prog/Main.java
@@ -1,49 +1,40 @@
 package org.prog;
 public class Main {
 
+    /**
+     * #######99#########################
+     * ##################################
+     * #####redCar#######################
+     * ##################################
+     * ##################################
+     * ##################################
+     * ################yellowCar#########
+     * ##################################
+     * ##################################
+     *
+     * @param args
+     */
+
     public static void main(String[] args) {
-//        CarService carService = new CarService();
-//
-//        Car redCar = new Car();
-//        Car yellowCar = new Car();
-//
-//        redCar.color = "red";
-//        yellowCar.color = "yellow";
-//        System.out.println(redCar.color);
-//        System.out.println(yellowCar.color);
-//
-//        Car thatCar = redCar;
-//        System.out.println(thatCar.color);
-//        paintCar(thatCar);
-//        System.out.println(">>>>>>" + redCar.color);
-//        carService.paintCar(yellowCar);
-//        System.out.println(yellowCar.color);
+        String s = "s";
+        String s1 = "";
+        String s2 = null;
 
-        Car myCar = new Car();
-        BigCar bigCar = new BigCar();
-        ElectricCar electricCar = new ElectricCar();
-        Telega telega = new Telega();
-        BigTruck bigTruck = new BigTruck();
-        Truck truck = new Truck();
-        ElectricTruck electricTruck = new ElectricTruck();
+        System.out.println(s.length());
+        System.out.println(s1.length());
+        System.out.println(s2.length());
 
-//        myCar.drive();
-//        bigCar.drive();
-//        electricCar.drive();
+//        Car car = new Car();
+//        car.smth();
+//        car.goTo("Lviv");
+//        car.goTo("Dnipro", "Chernihiv");
+//        car.goTo("Kyiv", "Kharkiv", "Poltava");
+//        car.goTo("Odessa", "Kyiv", "Dnipro", "food");
+    }
 
-        followTheRoute(myCar);
-        followTheRoute(telega);
-        followTheRoute(truck);
-        followTheRoute(bigTruck);
-        followTheRoute(electricTruck);
-        followTheRoute2(electricTruck);
-
-        Car car = new Car();
-        car.goTo("Lviv");
-        car.goTo("Dnipro", "Chernihiv");
-        car.goTo("Kyiv", "Kharkiv", "Poltava");
-        car.goTo("Odessa", "Kyiv", "Dnipro", "food");
-        car.goTo("Odessa", "Kyiv", "Dnipro", "food", "return");
+    private static void increment(Integer input){
+        input += 1000;
+        System.out.println(input);
     }
 
     public static void paintCar(Car car) {
